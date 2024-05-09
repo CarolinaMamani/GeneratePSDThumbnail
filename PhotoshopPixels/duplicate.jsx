5a5a31ded0c8d4369b8326b12552c012facdf1aa
Carolina <mamanicarolina27@gmail.com>
2024-05-09 18:48:47 -0300

    exelente duplicado!

--- PhotoshopPixels/duplicate.jsx
+++ PhotoshopPixels/duplicate.jsx
@@ -38,11 +38,38 @@ function duplicateLayersToDestination(doc, layersToDuplicate, destinationName) {
     }
 }
 
+// Function to move layers down by a specified distance in the selected document
+function moveShadLayersDown(distance, selectedDoc) {
+    var shadLayers = [];
+    // Find and add "shad1" and "shad2" layers to the array
+    var shad1Layer = selectedDoc.artLayers.getByName("shad1");
+    var shad2Layer = selectedDoc.artLayers.getByName("shad2");
+    if (shad1Layer) shadLayers.push(shad1Layer);
+    if (shad2Layer) shadLayers.push(shad2Layer);
+
+    // Move layers down in the selected document
+    for (var i = 0; i < shadLayers.length; i++) {
+        shadLayers[i].translate(0, distance); // Move layer down by specified distance
+    }
+}
+
+
 // Function to handle button click events
 function buttonClickHandler(buttonName) {
     var doc = app.activeDocument; // Get the currently active document
     var layersToDuplicate = ["shad1", "shad2","light1", "light2"];
     duplicateLayersToDestination(doc, layersToDuplicate, buttonName);
+    
+    var destinationDoc = findDocumentWithNameAndExtension(buttonName, ".gif"); // Find the selected document
+    if (!destinationDoc) {
+        alert("Could not find a document with the specified name containing '" + buttonName + "' and ending with '.gif'.");
+        return;
+    }
+    app.activeDocument = destinationDoc; // Set the selected document as active
+
+    // Move layers "shad1" and "shad2" 6 pixels down in the selected document
+    moveShadLayersDown(6, destinationDoc);
+
     dialog.close(); // Close the dialog after selecting an option
 }
 
